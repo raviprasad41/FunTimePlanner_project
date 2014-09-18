3b1e56489482f72474ab97137515f0b142febe74
raviprasad41 <raviprasad41@gmail.com>
2014-09-17 21:44:31 -0700

    display user names, events and Interests matching users
    Created a template called matches

--- funtimeplanner/views.py
+++ funtimeplanner/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from funtimeplanner.models import FunInterest, FunEvent
+from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -11,5 +12,31 @@ def index(request):
     context_dict = {'interests': interest_list}
 
     return render_to_response('funtimeplanner/index.html', context_dict, context)
+
+def event_matches(request):
+    context = RequestContext(request)
+    user_list = User.objects.all()
+    event_list = FunEvent.objects.all()
+    user_interest_list = user_list[1].funinterest_set.all()
+    context_dict = {'users': user_list,
+                    'events': event_list,
+                    'user_interests': user_interest_list,
+                    }
+
+
+    return render_to_response('funtimeplanner/matches.html', context_dict, context)
+
+
+
+    #for user_interest in user_interest_list:
+
+
+
+
+
+
+
+
+
 
 
