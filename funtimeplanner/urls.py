1397adb7e91a82339bd6550fa7a9769d638f9caf
raviprasad41 <raviprasad41@gmail.com>
2014-09-26 19:54:16 -0700

    Java Script and HTML to display id
    Created a Template which uses Java Script and HTML to display the user
    id.
    http://127.0.0.1:8000/funtimeplanner/userlist/

--- funtimeplanner/urls.py
+++ funtimeplanner/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url
 from funtimeplanner import views
-
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         #url(r'^interest/(?P<interest_name_url>\w+/$', views.interest, name='interest'),
-
+        url(r'^event_matches/$', views.event_matches, name='event_matches'),
+        url(r'^userlist/$', views.userlist_display, name='userlist'),
         )
--- funtimeplanner/views.py
+++ funtimeplanner/views.py
@@ -10,25 +10,62 @@ def index(request):
     context = RequestContext(request)
     interest_list = FunInterest.objects.all()
     context_dict = {'interests': interest_list}
-
     return render_to_response('funtimeplanner/index.html', context_dict, context)
 
 def event_matches(request):
     context = RequestContext(request)
     user_list = User.objects.all()
-    event_list = FunEvent.objects.all()
-    user_interest_list = user_list[1].funinterest_set.all()
-    context_dict = {'users': user_list,
-                    'events': event_list,
-                    'user_interests': user_interest_list,
-                    }
 
+    output_list = usermatches(user_list)
+
+    context_dict = {'pairs': output_list,
+                    }
 
     return render_to_response('funtimeplanner/matches.html', context_dict, context)
 
+def usermatches(user_list):
+    output_list=[]
+    #3. For Every User, Display his matching interests
+    for user in user_list:
+        output_pair = {}
+        output_pair["matches"] = interestMatch(user)
+        output_pair["user"] = user
+        output_list.append(output_pair)
+    return output_list
 
 
-    #for user_interest in user_interest_list:
+def interestMatch(user):
+    output = {}
+    #2. For Every users interest, display his matching events
+    for user_interest in user.funinterest_set.all():
+        eventMatch(user_interest, output)
+    return output
+
+def eventMatch(user_interest, output):
+    event_list = FunEvent.objects.all()
+    #1. For Every event, if the users interest is in Events Description or Events Name, build a dictionary for such events
+    for event in event_list:
+        if user_interest.name in event.Description or user_interest.name in event.name:
+            output[event.id] = event
+
+def userlist_display(request):
+    user_list = User.objects.all()
+    context = RequestContext(request)
+    context_dict = {'users' : user_list
+    }
+    return render_to_response('funtimeplanner/userlist.html', context_dict, context)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
